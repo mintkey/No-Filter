484e407a25e17cf7f3819f9881f24663877cdb4a
mintkey <helendmpsy@gmail.com>
2020-10-17 01:30:37 -0400

    Initial Commit

---
+++ No Filter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  No Filter
+//
+//  Created by Helen Dempsey on 10/17/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ No Filter/No_FilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  No_FilterApp.swift
+//  No Filter
+//
+//  Created by Helen Dempsey on 10/17/20.
+//
+
+import SwiftUI
+
+@main
+struct No_FilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
